f06eae56c2ca1bcc784a77c2e3573d78b6e14590
hyunwoo-lee228 <31426692+hyunwoo-lee228@users.noreply.github.com>
2017-09-14 14:59:12 +0800

    Angular2+

---
+++ lib/bootstrap.ts
@@ -0,0 +1,8 @@
+import { NgModuleRef } from '@angular/core';
+import { platformBrowser } from '@angular/platform-browser';
+import { AppModule } from './app.module';
+import { AppModuleNgFactory } from '../compiled/lib/app.module.ngfactory';
+
+export function bootstrapRedoc():Promise<NgModuleRef<AppModule>> {
+  return platformBrowser().bootstrapModuleFactory(AppModuleNgFactory);
+}
